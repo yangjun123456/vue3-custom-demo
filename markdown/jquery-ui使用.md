7db3a6f910a21957fb9239e1677ac8f73e4cb661
yjun666 <yjun_163_email@163.com>
2022-04-20 15:02:52 +0800

    添加jquery-ui 的使用demo

---
+++ markdown/jquery-ui使用.md
@@ -0,0 +1,2 @@
+#### 在index.html 中引入了jquery 和 jquery-ui (jquery 小于等于1.8.0版本)
+1. 直接使用draggable 详细文件见jquery-ui 文件夹下demo
--- src/views/component-test/jquery-ui/draggable.vue
+++ src/views/component-test/jquery-ui/draggable.vue
@@ -1,6 +1,14 @@
 <template>
 	<div class="draggable">
-		<div id="draggable-1"></div>
+        <div class="inputs">
+            start:<el-input v-model="counts[0]"></el-input>
+            drag:<el-input v-model="counts[1]"></el-input>
+            stop:<el-input v-model="counts[2]"></el-input>
+        </div>
+		<div id="draggable-2">
+            draggable-2
+            <p style="background:#fff;">handle: "p"</p>
+        </div>
 	</div>
 </template>
 <script lang="ts">
@@ -9,12 +17,31 @@ declare const $: any;
 const DraggableCom = defineComponent({
     name: 'DraggableCom',
     setup(props, context) {
+        const counts = ref([0, 0, 0]);
         onMounted(() => {
             // console.log($);
-            $('#draggable-1').draggable({});
+            // $('#draggable-1').draggable({});
+            $('#draggable-2').draggable({
+                cursor: 'move', // 鼠标样式
+                containment: '.el-main', // 父级类名， 只能在el-main里边拖动
+                scroll: false, // 不允许出现滚动条
+                handle: 'p',
+                start: function () {
+                    // 开始
+                    counts.value[0]++;
+                },
+                drag: function () {
+                    // 拖动中
+                    counts.value[1]++;
+                },
+                stop: function () {
+                    // 停止
+                    counts.value[2]++;
+                }
+            });
             // debugger;
         });
-        return { props };
+        return { props, counts };
     }
 });
 export default DraggableCom;
@@ -22,14 +49,17 @@ export default DraggableCom;
 
 <style lang="scss" scoped>
 .draggable {
-    width: 100%;
-    height: 100%;
-	display: flex;
-	flex-wrap: wrap;
+	width: 100%;
+	height: 100%;
 }
-#draggable-1 {
-	width: 50px;
-	height: 50px;
+.inputs{
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
+#draggable-2 {
+	width: 200px;
+	height: 200px;
 	background-color: red;
 }
 </style>
