19968ee0aca1f8758ff38203c8ec7adea54d95b7
yjun666 <yjun_163_email@163.com>
2023-01-30 14:40:50 +0800

    添加table组件

---
+++ src/components/table/column.vue
@@ -0,0 +1,65 @@
+<template>
+    <el-table-column :fixed="col.fixed || false"
+        :prop="col.prop || ''"
+        :label="col.label || ''"
+        :width="col.width || '-'"
+        :minWidth="col.minWidth || computeMinWidth(col.label)"
+        :resizable="col.resizable || false"
+        :show-overflow-tooltip="col.isShowOverflowTooltip || false"
+        :align="col.align || 'left'">
+        <template slot="header"
+            slot-scope="scope">
+            <slot :name="col.slot_header"
+                :data="scope"
+                :col="col">
+                <div class="class_header_default"
+                    :class="['class_header_' + col.prop]">
+                    {{ col.label }}
+                </div>
+            </slot>
+        </template>
+        <template slot-scope="scope">
+            <slot :data="scope"
+                :col="col">
+                请配置正确的插槽 8</slot>
+        </template>
+        <template v-if="col[children]&&col[children].length">
+            <Column v-for="(item,k) in col[children]"
+                :key="k"
+                :col="item">
+                <template slot-scope="scope"
+                    slot="slot_custom_header">
+                    <slot :name="scope.col.slot_header"
+                        :data="scope.data"
+                        :col="scope.col">
+                        <div>请配置正确的插槽 4</div>
+                    </slot>
+                </template>
+                <template slot-scope="scope">
+                    <slot :data="scope.data"
+                        :col="scope.col">请配置正确的插槽 5</slot>
+                </template>
+            </Column>
+        </template>
+    </el-table-column>
+</template>
+<script>
+import { TableIndexColumnMixin } from './table';
+export default {
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: 'Column',
+	mixins: [TableIndexColumnMixin],
+	props: {
+		col: {
+			type: Object,
+			required: true
+		},
+		children: {
+			// child 字段， 子列保存所在数据的字段名称， 是children、list或者是别的， 是什么这里就传什么
+			type: String,
+			required: false,
+			default: 'children'
+		}
+	}
+};
+</script>
